e11dfb5fd6c70b1e2c36c92d6703f970a230dc13
lewishudson <pythonlewish@gmail.com>
2022-09-21 10:58:04 +1200

    Photography page navigation - lifestyle/pets/product

--- static/js/photography.js
+++ static/js/photography.js
@@ -54,25 +54,105 @@ close.addEventListener('click', e => {
 
 })
 
-// checks if mouse is hovering over the image, if so it adds a caption. It removes it is the mouse is off the image.
-// lightbox.addEventListener('mouseover', e => {
-//   if (e.target == e.currentTarget) {
-//     var check_p = document.querySelector('#lightbox p')
-//     if (check_p !== null) {
-//       lightbox.removeChild(check_p)
-//     }
-//     return
-//   }
-//   else {
-//     var check_p = document.querySelector('#lightbox p')
-//     if (check_p !== null) {
-//       return
-//     }
-//     else {
-//       const caption = document.createElement('p')
-//       caption.classList.add("lightbox-caption")
-//       caption.id = "caption"
-//       caption.innerHTML = "This is a placeholder"
-//       lightbox.appendChild(caption)
-//     }
-//   }})
+// Photography Selector
+// Get the container element
+var btnContainer = document.getElementById("selector-div")
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("photo-btn")
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active-btn");
+    current[0].className = current[0].className.replace(" active-btn", "");
+    this.className += " active-btn";
+  });
+}
+
+// Hide photos that are not in selection
+
+const lifestyleDivs = document.getElementsByClassName("lifestyle")
+const petsDivs = document.getElementsByClassName("pet")
+const productDivs = document.getElementsByClassName("product")
+const allDivs = document.getElementsByClassName("gallery-img")
+
+
+const allBtn = document.getElementById("all-btn")
+const lifestyleBtn = document.getElementById("lifestyle-btn")
+const petsBtn = document.getElementById("pets-btn")
+const productBtn = document.getElementById("product-btn")
+
+// when user clicks product shows images with product tag
+productBtn.onclick = function () {
+  for (var i = 0; i < productDivs.length; i++) {
+    productDivs[i].style.display = "block"
+  }
+
+
+  for (var i = 0; i < petsDivs.length; i++) {
+    if (petsDivs[i].classList.contains("product")) {
+      continue
+    }
+    petsDivs[i].style.display ="none"
+  }
+
+  for (var i = 0; i < lifestyleDivs.length; i++) {
+    if (lifestyleDivs[i].classList.contains("product")) {
+      continue
+    }
+    lifestyleDivs[i].style.display ="none"
+  }
+}
+
+
+// when user clicks lifestyle shows images with product tag
+lifestyleBtn.onclick = function () {
+  for (var i = 0; i < lifestyleDivs.length; i++) {
+    lifestyleDivs[i].style.display = "block"
+  }
+
+  for (var i = 0; i < petsDivs.length; i++) {
+    if (petsDivs[i].classList.contains("lifestyle")) {
+      continue
+    }
+    petsDivs[i].style.display ="none"
+  }
+
+  for (var i = 0; i < productDivs.length; i++) {
+    if (productDivs[i].classList.contains("lifestyle")) {
+      continue
+    }
+    productDivs[i].style.display ="none"
+  }
+}
+
+
+// when user clicks pets shows images with product tag
+petsBtn.onclick = function () {
+  for (var i = 0; i < petsDivs.length; i++) {
+    petsDivs[i].style.display = "block"
+  }
+
+  for (var i = 0; i < lifestyleDivs.length; i++) {
+    if (lifestyleDivs[i].classList.contains("pet")) {
+      continue
+    }
+    lifestyleDivs[i].style.display ="none"
+  }
+
+  for (var i = 0; i < productDivs.length; i++) {
+    if (productDivs[i].classList.contains("pet")) {
+      continue
+    }
+    productDivs[i].style.display ="none"
+  }
+}
+
+// when user clicks all shows images with all tags
+
+allBtn.onclick = function () {
+  for (var i = 0; i < allDivs.length; i++) {
+    allDivs[i].style.display = "block"
+  }
+}
